6e25abf9701d7fdd6b5741cb96704c113af62447
Daniel Trindade <dot@certi.org.br>
2024-07-19 23:53:36 -0400

    fix: alterando o algoritmo baseline para o que foi enviado pelo professor.

--- Main.java
+++ Main.java
@@ -3,23 +3,34 @@ import java.io.FileWriter;
 import java.io.IOException;
 public class Main {
   public static void main(String[] args) {
+    int ordem, repeticoes;
+    float totalTime1 = 0;
+    float totalTime2 = 0;
       try (BufferedWriter writer = new BufferedWriter(new FileWriter("resultado.txt", true))) {
-          for (int i = 3; i <= 11; i += 2) {
-              for (int j = 0; j < 3; j++) {
-                  MatrizQuadrada mat = new MatrizQuadrada(i);
-                  MatrizQuadrada mat2 = new MatrizQuadrada(i);
-                  MatrizQuadrada mat3 = new MatrizQuadrada(i);
-                  mat.inicializaRandomico();
-                  writer.write("Medida para a Matriz Quadrada de Ordem " + i + ": " + (j + 1) + "\n");
-                  mat.imprimeMatriz(writer);
-                  mat2.copiarValores(mat);
-                  mat3.copiarValores(mat);
-                  mat2.imprimirEstatisticasDeterminanteV1(writer);
-                  mat3.imprimirEstatisticasDeterminanteV2(writer);
-              }
+          for (ordem = 3; ordem <= 11; ordem += 2) {
+            for (repeticoes = 0; repeticoes < 3; repeticoes++) {
+                Matriz mat = new Matriz(ordem);
+                Matriz mat2 = new Matriz(ordem);
+                Matriz mat3 = new Matriz(ordem);
+                mat.inicializaRandomico();
+                mat.imprime();
+                writer.write("Medida para a Matriz Quadrada de Ordem " + ordem + ": " + (repeticoes + 1) + "\n");
+                mat.imprimeMatriz(writer);
+                mat2.copiarValores(mat);
+                mat3.copiarValores(mat);
+                totalTime1 += mat2.imprimirEstatisticasDeterminanteV1(writer);
+                totalTime2 += mat3.imprimirEstatisticasDeterminanteV2(writer);
+                writer.write("\n");
+            }
+            //colocar a media no arquivo de cada versao
+            writer.write("Tempo medio versão 1: " + totalTime1 / 3 + " ns\n");
+            writer.write("Tempo medio versão 2: " + totalTime2 / 3 + " ns\n\n");
+            totalTime1 = 0;
+            totalTime2 = 0;
           }
       } catch (IOException e) {
           e.printStackTrace();
       }
+
   }
 }
--- Matriz.java
+++ Matriz.java
@@ -2,130 +2,338 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.Random;
 
-public class Matriz {
-  private int[][] mat;
-  private int numLinhas;
-  private int numColunas;
-  Matriz(int novoNumLinhas, int novoNumColunas) {
-    this.setNumLinhas(novoNumLinhas);
-    this.setNumColunas(novoNumColunas);
-    mat = new int[this.getNumLinhas()][this.getNumColunas()];
-  }
+class Matriz{
+	private int[][] mat;
+	private int tamLinha;
+	private int tamColuna;
+	private int linhaComMaisZeros;
+	private int colunaComMaisZeros;
+	private int zerosLinha;
+	private int zerosColuna;
 
-  public void setNumLinhas (int novoNumLinhas) {
-    this.numLinhas = novoNumLinhas;
-  }
-
-  public int getNumLinhas() {
-    return numLinhas;
-  }
-
-  public void setNumColunas (int novoNumColunas) {
-    this.numColunas = novoNumColunas;
-  }
-
-  public int getNumColunas() {
-    return numColunas;
-  }
-
-  public void setElemento(int linha, int coluna, int novoValor) {
-    this.mat[linha][coluna] = novoValor;
-  }
-
-  public int getElemento(int linha, int coluna) {
-    return this.mat[linha][coluna];
-  }
-
-  public void inicializaRandomico(){
-		int contC,contL,entrada;
-		Random gerador = new Random();
-		for(contL = 0; contL < this.getNumLinhas(); contL++){
-			for(contC = 0; contC < this.getNumColunas(); contC++){
-				entrada = gerador.nextInt((this.getNumLinhas() * this.getNumColunas()) * 2);
-				this.setElemento(contL,contC,entrada);
-			}
-		}
+	Matriz(){
+		mat = new int[6][6];
+		this.setTamanhoLinha(6);	
+		this.setTamanhoColuna(6);
+	}
+	Matriz(int ordem) {
+		this.setTamanhoLinha(ordem);
+		this.setTamanhoColuna(ordem);
+		mat = new int[this.getTamanhoLinha()][this.getTamanhoColuna()];
 	}
 
-  public int linhaComMaisZeros() {
-    int linhaComMaisZeros = 0;
-    Vetor vet = new Vetor(this.getNumLinhas());
-    for(int i = 0; i < this.getNumLinhas(); i++) {
-      for(int j = 0; j < this.getNumColunas(); j++) {
-        if(this.getElemento(i, j) == 0) {
-          vet.setElemento(i, vet.getElemento(i) + 1);
-        }
-      }
-    }
-    linhaComMaisZeros = vet.getPosMax();
+	Matriz(int numLinhas, int numColunas){
+		this.setTamanhoLinha(numLinhas);	
+		this.setTamanhoColuna(numColunas);
+		mat = new int[this.getTamanhoLinha()][this.getTamanhoColuna()];
+	}
+
+
+	public int getValor(int indiceI,int indiceJ){
+		return mat[indiceI][indiceJ];
+	}	
+	
+	public void setValor(int indiceI,int indiceJ, int novoValor){
+		mat[indiceI][indiceJ] = novoValor;
+	}
+
+	public int getTamanhoLinha(){
+		return tamLinha;
+	}	
+	
+	public int getTamanhoColuna(){
+		return tamColuna;
+	}	
+
+	private void setTamanhoLinha(int novoValor){
+		tamLinha = novoValor;
+	}
+
+	private void setTamanhoColuna(int novoValor){
+		tamColuna = novoValor;
+	}
+
+	public void setLinhaComMaisZeros(int novaLinhaComMaisZeros) {
+    this.linhaComMaisZeros = novaLinhaComMaisZeros;
+  }
+
+  public int getLinhaComMaisZeros() {
     return linhaComMaisZeros;
   }
 
-  public int colunaComMaisZeros() {
-    int colunaComMaisZeros = 0;
-    Vetor vet = new Vetor(this.getNumColunas());
-    for(int i = 0; i < this.getNumLinhas(); i++) {
-      for(int j = 0; j < this.getNumColunas(); j++) {
-        if(this.getElemento(i, j) == 0) {
-          vet.setElemento(j, vet.getElemento(j) + 1);
-        }
-      }
-    }
-    colunaComMaisZeros = vet.getPosMax();
+  public void setColunaComMaisZeros(int novaColunaComMaisZeros) {
+    this.colunaComMaisZeros = novaColunaComMaisZeros;
+  }
+
+  public int getColunaComMaisZeros() {
     return colunaComMaisZeros;
   }
-  
-  //fazer uma função que verifica qual coluna ou linha tem mais zeros
-  //dizer se é linha ou coluna
-  public void linhaOuColunaComMaisZeros(){
-    int linhaComMaisZeros = this.linhaComMaisZeros();
-    int colunaComMaisZeros = this.colunaComMaisZeros();
-    int zerosLinha = 0;
-    int zerosColuna = 0;
-    for(int i = 0; i < this.getNumColunas(); i++) {
-      if(this.getElemento(linhaComMaisZeros, i) == 0) {
-        zerosLinha++;
-      }
-    }
-    for(int i = 0; i < this.getNumLinhas(); i++) {
-      if(this.getElemento(i, colunaComMaisZeros) == 0) {
-        zerosColuna++;
-      }
-    }
-    if(zerosColuna >= zerosLinha){
-      System.out.println("A coluna com mais zeros é: " + colunaComMaisZeros + ", com " + zerosColuna + " zero(s)");
-    }
-    else {
-      System.out.println("A linha com mais zeros é: " + linhaComMaisZeros + ", com " + zerosLinha + " zero(s)");
-    }
+
+  public void setZerosLinha(int novosZerosLinha) {
+    this.zerosLinha = novosZerosLinha;
+  }
+
+  public int getZerosLinha() {
+    return zerosLinha;
+  }
+
+  public void setZerosColuna(int novosZerosColuna) {
+    this.zerosColuna = novosZerosColuna;
+  }
+
+  public int getZerosColuna() {
+    return zerosColuna;
   }
 
 
-  public void imprimeMatriz(){
-    System.out.println("Matriz:");
-    for(int i = 0; i < this.getNumLinhas(); i++){
-      for(int j = 0; j < this.getNumColunas(); j++){
-        System.out.print(this.getElemento(i, j) + " ");
-      }
-      System.out.println();
-    }
-  }
+	public void imprime(){
+		int conti,contj;	
+		for(conti = 0; conti < this.getTamanhoLinha(); conti++){
+			System.out.println();
+			for(contj = 0; contj < this.getTamanhoColuna(); contj++){
+				System.out.print(this.getValor(conti,contj) + " ");
+			}
+		}
+		System.out.println();	
+	}
 
-  public void imprimeMatriz(BufferedWriter writer) throws IOException {
+	public void imprimeMatriz(BufferedWriter writer) throws IOException {
+		int conti,contj;
     writer.write("Matriz usada para a determinante:\n");
-    for(int i = 0; i < this.getNumLinhas(); i++){
-      for(int j = 0; j < this.getNumColunas(); j++){
-        writer.write(this.getElemento(i, j) + " ");
+    for(conti = 0; conti < this.getTamanhoLinha(); conti++){
+      for(contj = 0; contj < this.getTamanhoColuna(); contj++){
+        writer.write(this.getValor(conti, contj) + " ");
       }
       writer.write("\n");
     }
   }
 
-  public void copiarValores(Matriz mat) {
-    for(int i = 0; i < this.getNumLinhas(); i++) {
-      for(int j = 0; j < this.getNumColunas(); j++) {
-        this.setElemento(i, j, mat.getElemento(i, j));
-      }
-    }
-  }
+	public void inicializaRandomico(){
+		int conti,contj, novoValor;
+		int ordem = this.getTamanhoLinha();
+		Random gerador = new Random();
+		for(conti = 0; conti < this.getTamanhoLinha(); conti++){
+			for(contj = 0; contj < this.getTamanhoColuna(); contj++){
+				novoValor = gerador.nextInt(ordem * ordem);
+				this.setValor(conti,contj,novoValor);
+			}
+		}
+	}
+
+	public void copiarValores(Matriz mat){
+		this.setTamanhoLinha(mat.getTamanhoLinha());
+		this.setTamanhoColuna(mat.getTamanhoColuna());
+		for(int i = 0; i < this.getTamanhoLinha(); i++){
+			for(int j = 0; j < this.getTamanhoColuna(); j++){
+				this.setValor(i,j,mat.getValor(i,j));
+			}
+		}
+	}
+
+	private void linhaEColunaComMaisZeros() {
+		int linhaZeros = 0;
+		int colunaZeros = 0;
+		Vetor zerosPorLinha = new Vetor(this.getTamanhoLinha());
+		Vetor zerosPorColuna = new Vetor(this.getTamanhoColuna());
+		for(int i = 0; i < this.getTamanhoLinha(); i++) {
+			for(int j = 0; j < this.getTamanhoColuna(); j++) {
+				if(this.getValor(i, j) == 0) {
+					zerosPorLinha.setElemento(i, zerosPorLinha.getElemento(i) + 1);
+					zerosPorColuna.setElemento(j, zerosPorColuna.getElemento(j) + 1);
+				}
+			}
+		}
+		linhaZeros = zerosPorLinha.getPosMax();
+		colunaZeros = zerosPorColuna.getPosMax();
+		this.setLinhaComMaisZeros(linhaZeros);
+		this.setColunaComMaisZeros(colunaZeros);
+		this.setZerosLinha(zerosPorLinha.getElemento(linhaZeros));
+		this.setZerosColuna(zerosPorColuna.getElemento(colunaZeros));
+	}
+
+	// caso matriz nao quadrada, retorna -1
+	public int retorneOrdem(){
+		int numL, numC, ordem;
+		numL = this.getTamanhoLinha();
+		numC = this.getTamanhoColuna();
+		ordem = -1;
+		if(numL == numC){
+			ordem = numL;
+		}
+		return ordem;
+	}	
+
+	private int detOrdem1(Matriz mat){
+		return mat.getValor(0,0);
+	}
+	
+	private int detOrdem2(Matriz mat){
+		int diagonalP, diagonalI;
+		diagonalP = mat.getValor(0,0) * mat.getValor(1,1);		
+		diagonalI = mat.getValor(1,0) * mat.getValor(0,1);		
+		return (diagonalP - diagonalI);
+	}
+
+	private int calculaSinal(int indiceL, int indiceC){
+		int sinal;
+		sinal = -1;
+		if( ((indiceL + indiceC)% 2) == 0 ){
+			sinal = 1;
+		}
+		return sinal;		
+	}
+
+	public void copiaMatrizMaiorParaMenor(Matriz maior,Matriz menor,int isqn,int jsqn){
+		int contAi,contAj,contBi,contBj,temp,numL,numC;
+		numL = menor.getTamanhoLinha();
+		numC = menor.getTamanhoColuna();
+		contAi = 0;
+		for(contBi = 0; contBi < numL; contBi++){
+			if(contAi == isqn){
+				contAi++;
+			}
+			contAj = 0;
+			for(contBj = 0; contBj < numC; contBj++){
+				if(contAj == jsqn){
+					contAj++;
+				}
+				temp = maior.getValor(contAi,contAj);
+				menor.setValor(contBi,contBj,temp);
+				contAj++;
+			}
+			contAi++;
+		}
+	}
+
+	private int detOrdemN(Matriz mat){
+		int sinal,cofator,contC,numL,numC;
+		int resposta = 0;
+		int detTemp;
+		Matriz matmenor;
+		numL = mat.getTamanhoLinha();
+		numC = mat.getTamanhoColuna();
+		
+		for(contC = 0; contC < mat.getTamanhoColuna(); contC++){
+			cofator = mat.getValor(0,contC);
+			sinal = this.calculaSinal(0,contC);
+			matmenor = new Matriz(numL - 1, numC - 1);
+			this.copiaMatrizMaiorParaMenor(mat,matmenor,0,contC);
+			detTemp = matmenor.determinante();
+			resposta = resposta + (cofator * sinal * detTemp);
+		}
+		return (resposta);
+	}
+
+	private int detOrdemNV2(Matriz mat){
+		int sinal,cofator,contC,contL,numL,numC;
+		int resposta = 0;
+		int detTemp;
+		Matriz matmenor;
+		numL = mat.getTamanhoLinha();
+		numC = mat.getTamanhoColuna();
+		mat.linhaEColunaComMaisZeros();
+		int linhaComMaisZeros = mat.getLinhaComMaisZeros();
+    int colunaComMaisZeros = mat.getColunaComMaisZeros();
+    int zerosLinha = mat.getZerosLinha();
+		int zerosColuna = mat.getZerosColuna();
+		if(zerosColuna > zerosLinha) {
+			for(contL = 0; contL < mat.getTamanhoColuna(); contL++){
+				cofator = mat.getValor(contL, colunaComMaisZeros);
+				if(cofator != 0){
+					sinal = this.calculaSinal(contL, colunaComMaisZeros);
+					matmenor = new Matriz(numL - 1,numC - 1);
+					this.copiaMatrizMaiorParaMenor(mat,matmenor,contL,colunaComMaisZeros);
+					detTemp = matmenor.determinanteV2();
+					resposta = resposta + (cofator * sinal * detTemp);
+				}
+			}
+		}
+		else {
+			for(contC = 0; contC < mat.getTamanhoColuna(); contC++){
+				cofator = mat.getValor(linhaComMaisZeros, contC);
+				if(cofator != 0){
+					sinal = this.calculaSinal(linhaComMaisZeros, contC);
+					matmenor = new Matriz(numL - 1,numC - 1);
+					this.copiaMatrizMaiorParaMenor(mat, matmenor, linhaComMaisZeros, contC);
+					detTemp = matmenor.determinante();
+					resposta = resposta + (cofator * sinal * detTemp);
+				}
+			}
+		}
+		return (resposta);
+	}
+
+	public int determinante(){
+		int ordem;
+		int det;
+
+		ordem = this.retorneOrdem();
+		det = 0;
+
+		if(ordem > 0){
+			switch (ordem) {
+			    case 1:  det = this.detOrdem1(this);
+				     break;
+			    case 2:  det = this.detOrdem2(this);
+				     break;
+			    default: det = this.detOrdemN(this);
+				     break;
+			}
+			
+		}
+		else{
+			System.out.println("Matriz nao eh quadrada!! retornando 0");
+		}
+
+		return det;
+	}
+
+	public int determinanteV2(){
+		int ordem;
+		int det;
+
+		ordem = this.retorneOrdem();
+		det = 0;
+
+		if(ordem > 0){
+			switch (ordem) {
+			    case 1:  det = this.detOrdem1(this);
+				     break;
+			    case 2:  det = this.detOrdem2(this);
+				     break;
+			    default: det = this.detOrdemNV2(this);
+				     break;
+			}
+			
+		}
+		else{
+			System.out.println("Matriz nao eh quadrada!! retornando 0");
+		}
+
+		return det;
+	}
+	
+	public long imprimirEstatisticasDeterminanteV1(BufferedWriter writer) throws IOException {
+		long startTime = System.nanoTime();
+		int determinante = this.determinante();
+		long endTime = System.nanoTime();
+		long totalTime = endTime - startTime;
+		String resultado = "O determinante da matriz é: " + determinante + "\n" +
+											 "Tempo total em nanosegundos para a versão 1: " + totalTime + " ns\n";
+
+		writer.write(resultado);
+		return totalTime;
+	}
+
+	public long imprimirEstatisticasDeterminanteV2(BufferedWriter writer) throws IOException {
+		long startTime2 = System.nanoTime();
+		int determinante = this.determinanteV2();
+		long endTime2 = System.nanoTime();
+		long totalTime2 = endTime2 - startTime2;
+		String resultado = "O determinante da matriz é: " + determinante + "\n" +
+												"Tempo total em nanosegundos para a versão 2: " + totalTime2 + " ns\n\n";
+		writer.write(resultado);
+		return totalTime2;
+	}
+	
 }
